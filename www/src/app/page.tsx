5e782d04439b8a3d87d8b7791e52c0c907c5668d
Wouter De Loose <wouter@woutr.me>
2024-11-02 18:14:37 +0700

    Updated layout

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -1,101 +1,89 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	return (
+		<>
+			<header className="bg-[#e6f3f3]">
+				<div className="container mx-auto px-4">
+					<div className="py-6">
+						<h1>
+							<a href="/" title="Hair compare" className="text-2xl text-black">
+								HairCompare.
+							</a>
+						</h1>
+					</div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+					<div className="flex flex-col md:flex-row items-center justify-between mt-10">
+						<div className="md:w-2/4 mb-8 md:mb-0">
+							<h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-4">
+								We find the best clinics for your hair transplant surgery
+							</h1>
+
+							<p className="text-gray-600 mb-6">
+								Expert doctors ready to help you achieve the hair you&apos;ve always wanted.
+							</p>
+
+							<button className="bg-purple-600 hover:bg-purple-700 text-white rounded-lg px-5 py-3 flex items-center gap-2">
+								Find your clinic
+								<svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+								</svg>
+							</button>
+						</div>
+
+						<div className="md:w-1/2 flex justify-end">
+							<img src="/header-right.png" alt="" title="" />
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<main>
+				<div className="container mx-auto px-4 py-10">
+				</div>
+			</main>
+
+			<footer className="bg-[#e6f3f3]">
+				<div className="container mx-auto px-4 py-10">
+					<div className="mb-14 flex items-center">
+						<div>
+							<h1>
+								<a href="/" title="Hair compare" className="text-2xl text-black">
+									HairCompare.
+								</a>
+							</h1>
+							<div className="text-slate-900">Helping you find the best hair transplant clinics worldwide.</div>
+						</div>
+
+						<div className="ml-auto">
+							<form action="/">
+								<div className="flex space-x-5">
+									<input type="text" placeholder="Enter your email" className="border border-gray-300 rounded p-2 w-[300px]" />
+									<button type="submit" className="bg-slate-700 hover:bg-slate-900 text-white rounded px-5 py-2">Subscribe to Newsletter</button>
+								</div>
+							</form>
+						</div>
+					</div>
+
+					<div className="grid grid-cols-3">
+						<div>
+							<h3 className="text-slate-900 text-xl font-semibold mb-5">About Us</h3>
+
+							<div className="flex flex-col text-slate-900">
+								<a href="/" title="Our Mission" className="hover:underline">Our Mission</a>
+							</div>
+						</div>
+
+						<div>
+							<h3 className="text-slate-900 text-xl font-semibold mb-5">Resources</h3>
+
+							<div className="flex flex-col text-slate-900">
+								<a href="/" title="Hair Loss Guide" className="hover:underline">Hair Loss Guide</a>
+								<a href="/" title="Treatment Options" className="hover:underline">Treatment Options</a>
+								<a href="/" title="Hair Loss Guide" className="hover:underline">Hair Loss Guide</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</>
+	);
 }
